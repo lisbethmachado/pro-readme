7c918fe3ad627615fd574b7e1bfc8562b38b4d48
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-07-06 15:02:05 -0700

    fixed github link and indentation

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const fs = require("fs");
 const questions = [
   {
     type: "input",
-    message: "Enter your name:",
-    name: "name",
+    message: "Enter your Github username:",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "Enter your primary email:",
+    name: "email",
   },
   {
     type: "input",
@@ -27,47 +32,68 @@ const questions = [
     message: "Provide instructions and examples for use:",
     name: "usage",
   },
+  {
+    type: "list",
+    message: "Which is your preferred license?",
+    name: "license",
+    choices: [
+      "Mozilla",
+      "The MIT",
+      "The Artistic"
+    ]
+  },
+  {
+    type: "input",
+    message: "Give instructions for contributing:",
+    name: "contributing"
+  },
 ];
 
 function init() {
   inquirer.prompt(questions).then((answers) => {
-    const writeFile = `
-      ## ${answers.title}
+    const writeFile =
+      `## ${answers.title}
 
-      ## Description
-      ${answers.description}
+## Description
+${answers.description}
 
-      ## Table of Contents    
-      * [Installation](#installation)
-      * [Usage](#usage)
-      * [Credits](#credits)
-      * [License](#license)
+## Table of Contents    
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
       
-      ## Installation
-      ${answers.installation}
+## Installation
+${answers.installation}
 
-      ## Usage 
-      ${answers.usage}
+## Usage 
+${answers.usage}
+      
+## License
+      
+## Badges
 
-      ## Credits
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic%202.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)
       
-      ## License
-      
-      ## Badges
-      
-      ## Contributing
-      
-      `;
-    
+## Contributing
+
+## Tests
+
+## Questions
+For more information visit [http://www.github.com/${answers.github}]. You may contact ${answers.email} with any questions or suggestions you may have. Thank you for your interest!`;
+
     fs.writeFile(answers.title + ".md", writeFile, (error) => {
       if (error) {
         return console.log(error);
       }
       console.log("Success!");
-    
+
     });
-    });
-  }
+  });
+}
 init();
 
 
